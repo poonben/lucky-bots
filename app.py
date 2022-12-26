dbf494a2b085a6441e05e20b20f050ff5f5af78f
poonben <93625450+poonben@users.noreply.github.com>
2022-12-27 06:16:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from linebot import (
 
 app = Flask(__name__)
 
-lineaccesstoken = 'KkIIUDSiGrMEoeUi+iRiZNfY5Gce7zyINhGfZbR0ln+VcCZTDRg3E01IlU5QuERIQ7SABUzH/J6G7ReeCFlgM0xQG388iOrY4e5WKZ6m2rOR/6Twg8NeWPHAejcLnEbQnlG5uMeZC+9Vgx/ExbOp2QdB04t89/1O/w1cDnyilFU='
+lineaccesstoken = 'WupG+Oqu6t+bgHe83W5Vdp1Y/8ceP0nDbVdcfQwB/VnUOlUBXzWEB+gj8nOwei5kFDTkevc7zj7pUn32pg7LcvPFOH0mynFRz/rlQWvjzIlJoFzULJJ/N67BOaduMjqk4yJiXXw+81rfsfcPKEzsIgdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(lineaccesstoken)
 
 casedata = pd.read_excel('casedata.xlsx')
